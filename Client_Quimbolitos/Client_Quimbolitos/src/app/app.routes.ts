98cc5d1e7b1a2b20061794d4b508307cb77506d6
Kevin Mateo Simbaña Merchán <mateomerchan1@outlook.com>
2025-11-29 22:21:17 +0100

    Primer diseño Login

---
+++ Client_Quimbolitos/Client_Quimbolitos/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes, withRouterConfig } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' } 
+];
